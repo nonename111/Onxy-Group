065641e7e19b871504b9d66c6c73a81b92069e1d
IRVAN KURNIAWAN <94360236+nonename111@users.noreply.github.com>
2024-08-26 19:17:17 +0700

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,15 +1,30 @@
 function toggleSidebar() {
-    const sidebar = document.querySelector(".sidebar");
-    const content = document.querySelector(".content");
+  const sidebar = document.querySelector(".sidebar");
+  const content = document.querySelector(".content");
+  const toggleBtnIcon = document.querySelector(".toggle-btn i");
 
-    sidebar.classList.toggle("hide");
-    content.classList.toggle("full-width");
+  sidebar.classList.toggle("hide");
+  content.classList.toggle("full-width");
+
+  // Ganti ikon antara bars dan times saat sidebar ditampilkan/disembunyikan
+  if (sidebar.classList.contains("hide")) {
+      toggleBtnIcon.classList.remove("fa-times");
+      toggleBtnIcon.classList.add("fa-bars");
+  } else {
+      toggleBtnIcon.classList.remove("fa-bars");
+      toggleBtnIcon.classList.add("fa-times");
   }
+}
 
-  document.addEventListener("DOMContentLoaded", function () {
-    const sidebar = document.querySelector(".sidebar");
-    const content = document.querySelector(".content");
+document.addEventListener("DOMContentLoaded", function () {
+  const sidebar = document.querySelector(".sidebar");
+  const content = document.querySelector(".content");
+  const toggleBtnIcon = document.querySelector(".toggle-btn i");
 
-    sidebar.classList.add("hide");
-    content.classList.add("full-width");
-  });
+  sidebar.classList.add("hide");
+  content.classList.add("full-width");
+
+  // Pastikan ikon diubah sesuai dengan kondisi sidebar pada load pertama kali
+  toggleBtnIcon.classList.remove("fa-times");
+  toggleBtnIcon.classList.add("fa-bars");
+});
